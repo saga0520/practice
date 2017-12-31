d39d9c07d94ae865e961e1d9a38642295caf726b
saga <saga0520@example.com>
2017-12-31 17:46:49 +0900

    ArrayListに修正

--- github/BlackJack.java
+++ github/BlackJack.java
@@ -1,57 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BlackJack {
 	public static void main(String args[]) throws IOException{
-		//最初の二枚のカードをramdomメソッドで生成
+		//最初の2枚のカードをramdomメソッドで生成
 		int f = (int)(Math.random()*10)+1;
 		int b = (int)(Math.random()*10)+1;
 		//カードの合計
 		int total = f + b ;
-		//追加で引くカードを19回分用意する
-		int card[] = new int[19];
 		//最初の手を出力する
-		System.out.println("一枚目のカード : " + f);
-		System.out.println("二枚目のカード : "  + b);
+		System.out.println("1枚目のカード : " + f);
+		System.out.println("2枚目のカード : "  + b);
 		System.out.println("あなたの手 : "+ total);
+		//追加で受け取るカードを格納するためにArrayListを用意する
+		ArrayList<Integer> array = new ArrayList<Integer>();
 
-		for(int i =0 ; i < 20; i++) {
-			//追加のカードをramdomメソッドで生成
-			card[i] = (int)(Math.random()*10)+1;
-			//追加のカードの点数を足す
-			total = total + card[i];
+		//追加で受け取るカードの制限回数なしと想定する
+		for(int i = 0 ; ; i++) {
+			//追加で受け取ったカードをramdomメソッドで生成
+			int card  = (int)(Math.random()*10)+1;
+			//addメソッドで追加で受け取ったカードをArrayListに格納する
+			array.add(card);
+			//追加で受け取ったカードをgetメソッドでArrayListから取り出して合計点数に足す
+			total = total + array.get(i);
 			System.out.println("更にカードを受け取りますか？");
 			System.out.println("HIT or STAND を入力してEnter ");
 			//カードを受け取るかの判断をするメッドを呼び出し、判定を受け取る
 			String judge = getScore();
 
-				//HITを選択し、カードを受け取った場合のコメントを出力
-				if (judge.equals("HIT")) {
-					System.out.println("HIT!!" + "\n");
-					System.out.println("もう一枚カードを受け取ります");
-					System.out.println(i + 2 + "枚目のカード : " + card[i]);
-					//合計点数を出力する
-					System.out.println("あなたの合計点数 : " + total );
-					//合計点数別のコメント用メソッドを呼び出す
-					String result = BlackJack.getTotal(total);
-					System.out.println(result);
-						//合計点数が21以上なら終了する
-						if (total >= 21) {
-							break;
-						}
-				//受け取らなかった場合の合計を出力
-				} else if(judge.equals("STAND")){
-					System.out.println("STAND!!");
-					System.out.println("あなたの合計点数 : " + (total - card[i]));
-					//合計点数別のコメント用メソッドを呼び出す
-					String result = BlackJack.getTotal(total - card[i]);
-					System.out.println(result);
-					break;
-				} else {
-					System.out.println("不正行為は禁止されています");
-					break;
-				}
+			//HITを選択し、カードを受け取った場合のコメントを出力
+			if (judge.equals("HIT")) {
+				System.out.println("HIT!!" + "\n");
+				System.out.println("もう1枚カードを受け取ります");
+				System.out.println(i + 3 + "枚目のカード : " + card);
+				//合計点数を出力する
+				System.out.println("あなたの合計点数 : " + total );
+				//合計点数別のコメント用メソッドを呼び出す
+				String result = BlackJack.getTotal(total);
+				System.out.println(result);
+					//合計点数が21以上なら終了する
+					if (total >= 21) {
+						break;
+					}
+			//STANDを選択し、カードを受け取らなかった場合の合計点数を出力
+			} else if(judge.equals("STAND")){
+				System.out.println("STAND!!");
+				System.out.println("あなたの合計点数 : " + (total - card));
+				//合計点数別のコメント用メソッドを呼び出す
+				String result = BlackJack.getTotal(total - card);
+				System.out.println(result);
+				break;
+			} else {
+				System.out.println("不正行為は禁止されています");
+				break;
+			}
 		}
 	}
 
