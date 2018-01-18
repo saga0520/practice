12cabadf30545878f0a02eddcb4a0cdd04f95497
saga <saga0520@example.com>
2018-01-18 23:40:55 +0900

    出羽さん指摘修正

--- github/BlackJack.java
+++ github/BlackJack.java
@@ -10,17 +10,17 @@ public class BlackJack {
 		/**
 		 *	最初の2枚のカードをramdomメソッドで生成
 		 */
-		int card_1 = (int)(Math.random()*10)+1;
-		int card_2 = (int)(Math.random()*10)+1;
+		int myCard_1 = (int)(Math.random()*10)+1;
+		int myCard_2 = (int)(Math.random()*10)+1;
 		/**
 		 *	カードの合計
 		 */
-		int total = card_1 + card_2 ;
+		int total = myCard_1 + myCard_2 ;
 		/**
 		 *	最初の手を出力する
 		 */
-		System.out.println("1枚目のカード : " + card_1);
-		System.out.println("2枚目のカード : "  + card_2);
+		System.out.println("1枚目のカード : " + myCard_1);
+		System.out.println("2枚目のカード : "  + myCard_2);
 		System.out.println("あなたの手 : "+ total);
 		/**
 		 *	追加で受け取るカードを格納するためにArrayListを用意する
@@ -34,11 +34,11 @@ public class BlackJack {
 			/**
 			 *	追加で受け取ったカードをramdomメソッドで生成
 			 */
-			int card_add  = (int)(Math.random()*10)+1;
+			int addCard  = (int)(Math.random()*10)+1;
 			/**
 			 *	addメソッドで追加で受け取ったカードをArrayListに格納する
 			 */
-			array.add(card_add);
+			array.add(addCard);
 			/**
 			 *	追加で受け取ったカードをgetメソッドでArrayListから取り出して合計点数に足す
 			 */
@@ -56,7 +56,7 @@ public class BlackJack {
 			if (judge.equals("HIT")) {
 				System.out.println("HIT!!" + "\n");
 				System.out.println("もう1枚カードを受け取ります");
-				System.out.println(i + 3 + "枚目のカード : " + card_add);
+				System.out.println(i + 3 + "枚目のカード : " + addCard);
 				/**
 				 *	合計点数を出力する
 				 */
@@ -77,11 +77,11 @@ public class BlackJack {
 			 */
 			} else if(judge.equals("STAND")){
 				System.out.println("STAND!!");
-				System.out.println("あなたの合計点数 : " + (total - card_add));
+				System.out.println("あなたの合計点数 : " + (total - addCard));
 				/**
 				 *	合計点数別のコメント用メソッドを呼び出す
 				 */
-				String result = BlackJack.getTotal(total - card_add);
+				String result = BlackJack.getTotal(total - addCard);
 				System.out.println(result);
 				break;
 			} else {
@@ -92,22 +92,21 @@ public class BlackJack {
 	}
 
 	/**
-	 *	カードを受け取るかを判断するメソッド
-	 *	コンソール画面でHITかSTANDを入力し、入力した値を返す
+	 *	コンソール画面で入力された値を返すメソッド
 	 */
 	public static String getScore() throws IOException {
-		String jg;
+		String inputChar;
 		/**
-		 *	コンソール画面に入力された文字を取得する
+		 *	BufferedReaderオブジェクトを生成し、コンソール画面から一行読み込む
 		 */
 		BufferedReader br =
 			new BufferedReader(new InputStreamReader(System.in));
-				jg = br.readLine();
-	return jg;
+				inputChar = br.readLine();
+	return inputChar;
 	}
 
 	/**
-	 *	合計点数によって出力するコメントを判定する
+	 *	合計点数によって出力するコメントを判定するメソッド
 	 */
 	public static String getTotal(int subtotal) {
 		String result;
